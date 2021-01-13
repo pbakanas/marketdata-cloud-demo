4dc0e3f5a729de84415d770399c0b05a5acbcbe8
Chris Matta <chris@confluent.io>
2021-01-13 20:41:55 +0000

    Modified setup-topics.sh to use the .env credentials

--- scripts/setup-topics.sh
+++ scripts/setup-topics.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
+DIR=$(cd $(dirname $0) && pwd)
+
+if [[ ! -f $DIR/../.env ]]; then
+	echo "Error: missing .env file in the root of this demo, please see the README"
+	exit 1
+fi
+
+source $DIR/../.env
+echo ${BOOTSTRAP_SERVERS}
+
+if [[ ! -f $DIR/client.properties ]]; then
+	echo "client.properties not found, generating..."
+	cat <<EOF> ${DIR}/client.properties
+		bootstrap.servers=${BOOTSTRAP_SERVERS}
+		security.protocol=SASL_SSL
+		sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username='${API_KEY}'   password='${API_SECRET}';
+		sasl.mechanism=PLAIN
+		client.dns.lookup=use_all_dns_ips
+EOF
+fi
 
 TOPICS=(orders marketdata)
 
 for topic in ${TOPICS[*]}; do
-    docker-compose exec kafka \
-        kafka-topics --bootstrap-server kafka:9091 \
+    docker run --rm \
+	-v ${DIR}/client.properties:/client.properties \
+	confluentinc/cp-kafka:latest \
+        kafka-topics --bootstrap-server ${BOOTSTRAP_SERVERS} \
+	--command-config /client.properties \
         --create \
         --topic ${topic} \
-        --partitions 1 \
-        --replication-factor 1
+        --partitions 1
 done
